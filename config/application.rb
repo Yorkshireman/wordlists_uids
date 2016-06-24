416d5d65df245e03a60d686f55bb16a77583275b
Andrew Stelmach <Andrew.Stelmach@sky.uk>
2016-06-24 21:40:08 +0100

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module WordlistsUids
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
